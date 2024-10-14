d8c25226033105e1a113d46e49245c591a9550e6
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-10-14 12:26:03 +0200

    paintings

--- app/Http/Controllers/PaintingController.php
+++ app/Http/Controllers/PaintingController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PaintingController extends Controller
 {
+
+    public $paintings;
+
+
+    public function __construct() {
+        $this ->paintings = Storage::json('Painting.json');
+        // dd($this->paintings);
+    }
+
     public function index()
     {
-        return "it works!";
+        return view('index', ['paintings' => $this->paintings]);
     }
 }
